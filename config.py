7b945d597ab628c9059140bf8d3cfbe114e0c678
Joris <joris.bertram@student.uhasselt.be>
2022-11-17 15:59:55 +0100

    Start of User(db) class and flask_login Login Manager

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 # TODO: Modify to our needs
 class Config:
@@ -11,11 +12,15 @@ class Config:
 class DevelopmentConfig(Config):
    DEBUG=True
    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/flaskapp'  # <--- insert our DB URI
+   # REMEMBER_COOKIE_DURATION = timedelta(seconds=30)       # Time before cookie expires
+   # PERMANENT_SESSION_LIFETIME = timedelta(seconds=30)     # Can be linked to cookie duration?
 
 class TestingConfig(Config):
    DEBUG = True
    TESTING = True
    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/flaskapp'  # <--- insert our DB URI
+   # REMEMBER_COOKIE_DURATION = timedelta(seconds=30)       # Time before cookie expires
+   # PERMANENT_SESSION_LIFETIME = timedelta(seconds=30)     # Can be linked to cookie duration?
 
 
 config = {
