d846b45c586e883d81cbd1f045b7140cd397c7b0
Brent Zoomers <brent.zoomers@student.uhasselt.be>
2022-12-01 20:36:54 +0100

    C Testing matching algo

--- backend_REST/models/vacancy.py
+++ backend_REST/models/vacancy.py
@@ -5,6 +5,21 @@ from rdflib.namespace import RDF, RDFS, FOAF, XSD
 
 from pandas import DataFrame
 
+# Vacancy (id) :
+#     id has a jobTitle
+#     id has a startDate
+#     id has a endDate
+#     id has a availability
+#     id has a owner (enterprise)
+
+#     id has a Diploma
+#     id has Skills[]
+#     id has Languages[]
+#     id has a jobDescription
+#     id has a jobResponsibilities
+#     id has a jobSalary
+#     id has a jobLocation
+
 class Vacancy():
     def add():
         pass
