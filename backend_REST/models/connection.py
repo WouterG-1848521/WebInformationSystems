c905ec5c1534f136cf3422dc8c669008a3bedbd4
Joris Bertram <joris.bertram@student.uhasselt.be>
2022-12-04 23:28:53 +0100

    Added connection request routes, Added DBConnectionRequest table, Added DB functionality for connection request routes [accepting request still needs to be linked with RDF graph action]

--- backend_REST/models/connection.py
+++ backend_REST/models/connection.py
@@ -1,3 +1,4 @@
+from backend_REST import db
 from backend_REST.graph import LOCAL
 
 from rdflib import Literal, RDF, URIRef
@@ -5,9 +6,57 @@ from rdflib.namespace import RDF, RDFS, FOAF, XSD
 
 from backend_REST.graph import LOCAL, PERSON
 
+from backend_REST.models.database import DBConnectionRequest
+
 from pandas import DataFrame
 
 class Connection():
+    def send_request(from_user_id, to_user_id):
+        # check if already exists
+        requests = db.session.query(DBConnectionRequest).filter(DBConnectionRequest.fromUser==from_user_id, DBConnectionRequest.toUser==to_user_id).first()
+        if (requests != None):
+            return -1
+        
+        # Add to DB
+        request = DBConnectionRequest()
+        request.fromUser = from_user_id
+        request.toUser = to_user_id
+        
+        db.session.add(request)
+        db.session.commit()
+        
+        return request.id
+        
+    
+    def cancel_request(request_id):
+        # Delete from DB
+        request = DBConnectionRequest.query.get(request_id)
+
+        if (request != None):
+            db.session.delete(request)
+            db.session.commit()
+            
+    
+    def accept_request(request_id):
+        # TODO: add to both user?
+        Connection.cancel_request(request_id)
+        
+        
+    def deny_request(request_id):
+        Connection.cancel_request(request_id)
+    
+    
+    def get_pending_requests_by_user(user_id):
+        requests = db.session.query(DBConnectionRequest).filter(DBConnectionRequest.toUser==user_id).all()
+        
+        pending_requests = []
+        for request in requests:
+            pending_requests.append(request.fromUser)
+        return pending_requests
+        
+        
+        
+        
     def add_to_user(graph, user1_id, user2_id):
 
         # TODO : Check if users exist
