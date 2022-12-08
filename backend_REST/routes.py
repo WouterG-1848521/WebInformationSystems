d83171edaa71832620ce3e866328817f72073cac
Joris Bertram <joris.bertram@student.uhasselt.be>
2022-12-08 16:20:44 +0100

    Added seperate matching route file with some basic routes

--- backend_REST/routes.py
+++ backend_REST/routes.py
@@ -4,6 +4,7 @@ from backend_REST.user_routes import create_user_routes
 from backend_REST.enterprise_routes import create_enterprise_routes
 from backend_REST.vacancy_routes import create_vacancy_routes
 from backend_REST.connection_routes import create_connections_routes
+from backend_REST.matching_routes import create_matching_routes
 
 
 def create_routes(app, g):
@@ -13,3 +14,4 @@ def create_routes(app, g):
     create_enterprise_routes(app, g)
     create_vacancy_routes(app, g)
     create_connections_routes(app, g)
+    create_matching_routes(app, g)
