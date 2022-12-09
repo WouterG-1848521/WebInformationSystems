b680ad666434410f8f1f5dbd7e28d244dc59eba7
Joris Bertram <joris.bertram@student.uhasselt.be>
2022-12-09 15:38:27 +0100

    Added skill and language to wikidata test

--- wikidata_test.py
+++ wikidata_test.py
@@ -3,20 +3,67 @@ import pandas as pd
 
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
-query = """
-SELECT ?profession ?professionLabel
-WHERE {
-    ?profession wdt:P31 wd:Q28640 .
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
-} 
-LIMIT 3
-"""
 
-sparql.setQuery(query)
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+def get_professions():
+    query = """
+    SELECT ?profession ?professionLabel
+    WHERE {
+        ?profession wdt:P31 wd:Q28640 .
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+    } 
+    LIMIT 3
+    """
 
-results_df = pd.io.json.json_normalize(results['results']['bindings'])
-results_df[['profession.value', 'profession.value']].head()
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-print(results_df.to_json())
+    results_df = pd.io.json.json_normalize(results['results']['bindings'])
+    results_df[['profession.value', 'profession.value']].head()
+
+    print(results_df)
+
+
+def get_skills():
+    query = """
+    SELECT ?skill ?skillLabel
+    WHERE {
+        ?skill wdt:P31 wd:Q205961 .
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+    } 
+    LIMIT 3
+    """
+
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    results_df = pd.io.json.json_normalize(results['results']['bindings'])
+    results_df[['skill.value', 'skill.value']].head()
+
+    print(results_df)
+
+
+def get_languages():
+    query = """
+    SELECT ?language ?languageLabel
+    WHERE {
+        ?language wdt:P31 wd:Q34770 .
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+    } 
+    LIMIT 3
+    """
+
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    results_df = pd.io.json.json_normalize(results['results']['bindings'])
+    results_df[['language.value', 'language.value']].head()
+
+    print(results_df)
+
+
+# get_professions()
+# get_skills()
+get_languages()
