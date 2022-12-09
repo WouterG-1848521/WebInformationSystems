7a6cc8600c2537721ad5530169b2219b0e3f020e
Joris Bertram <joris.bertram@student.uhasselt.be>
2022-12-09 17:21:48 +0100

    Added list of languages, skills and professions, Added queries which got the lists

--- wikidata_test.py
+++ wikidata_test.py
@@ -9,39 +9,57 @@ def get_professions():
     SELECT ?profession ?professionLabel
     WHERE {
         ?profession wdt:P31 wd:Q28640 .
+        ?profession wikibase:sitelinks ?linkcount .
         SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+        FILTER (?linkcount >= 60 )
     } 
-    LIMIT 3
+    GROUP BY ?profession ?professionLabel ?linkcount
+    ORDER BY DESC(?linkcount)
+    LIMIT 50
     """
 
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
-    results_df = pd.io.json.json_normalize(results['results']['bindings'])
-    results_df[['profession.value', 'profession.value']].head()
-
+    results_df = pd.json_normalize(results['results']['bindings'])
     print(results_df)
+    results_json = results_df[['profession.value',
+                               'professionLabel.value']].to_json()
+    print(results_json)
+
+    # Write pretty print JSON data to file
+    with open("professions.json", "w") as outfile:
+        outfile.write(results_json)
 
 
 def get_skills():
     query = """
     SELECT ?skill ?skillLabel
     WHERE {
-        ?skill wdt:P31 wd:Q205961 .
+        ?skill wdt:P31 ?Q205961 .
+        ?skill wikibase:sitelinks ?linkcount .
         SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+        FILTER (?linkcount >= 1)
     } 
-    LIMIT 3
+    GROUP BY ?skill ?skillLabel ?linkcount
+    ORDER BY DESC(?linkcount)
+    LIMIT 50
     """
 
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
-    results_df = pd.io.json.json_normalize(results['results']['bindings'])
-    results_df[['skill.value', 'skill.value']].head()
-
+    results_df = pd.json_normalize(results['results']['bindings'])
     print(results_df)
+    results_json = results_df[['skill.value',
+                               'skillLabel.value']].to_json()
+    print(results_json)
+
+    # Write pretty print JSON data to file
+    with open("skills.json", "w") as outfile:
+        outfile.write(results_json)
 
 
 def get_languages():
@@ -49,21 +67,30 @@ def get_languages():
     SELECT ?language ?languageLabel
     WHERE {
         ?language wdt:P31 wd:Q34770 .
+        ?language wikibase:sitelinks ?linkcount .
         SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+        FILTER (?linkcount >= 100) .
     } 
-    LIMIT 3
+    GROUP BY ?language ?languageLabel ?linkcount
+    ORDER BY DESC(?linkcount)
+    LIMIT 50
     """
 
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
-    results_df = pd.io.json.json_normalize(results['results']['bindings'])
-    results_df[['language.value', 'language.value']].head()
+    results_df = pd.json_normalize(results['results']['bindings'])
+    results_json = results_df[['language.value',
+                               'languageLabel.value']].to_json()
 
-    print(results_df)
+    print(results_json)
+    # Write pretty print JSON data to file
+
+    with open("languages.json", "w") as outfile:
+        outfile.write(results_json)
 
 
 # get_professions()
-# get_skills()
-get_languages()
+get_skills()
+# get_languages()
